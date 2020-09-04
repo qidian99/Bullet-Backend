bc2b5da1513cdac4767a1313d8a6b78ce78b54dd
qidian99 <qidian990107@163.com>
2020-09-04 09:04:24 -0700

    Added room schema for Bullet

---
+++ models/room.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const RoomSchema = new Schema({
+  alias: { type: String, required: true }, // The nickname of the room
+  users: { type: Array, required: true }, // All the users in the room
+  admins: { type: Array, require: true }, // At least one admin for a room
+  pending: { type: Array, required: false }, // Pending list for all the users waiting to join the room
+  public: { type: Boolean, required: false }, // Whether the room is public or not
+});
+
+module.exports = mongoose.model('Room', RoomSchema);
