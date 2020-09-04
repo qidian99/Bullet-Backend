ae2ce654dc018d97601419ca41b38d0b98df22e1
qidian99 <qidian990107@163.com>
2020-09-04 13:35:10 -0700

    Added invitation resolvers

---
+++ graphql/schema/invitation.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server');
+
+const typedef = gql`
+	type Invitation {
+		invitationId: ID!
+		user: User!
+		room: Room
+		accepted: Int!
+		sentAt: String!
+	}
+
+	extend type Query {
+		Invitations: [Invitation],
+		InvitationByUser(user: ID!): [Invitation]
+		findAllInvitationsInRoom(room: ID!): [Invitation]
+	}
+
+	extend type Mutation {
+		createRoomInvitation(roomId: ID! userId: ID!): Invitation!
+		acceptRoomInvitation(invitationId: ID!): Invitation!
+	}
+`
+
+module.exports = typedef;
